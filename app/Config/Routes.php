900fa15d976ed96b504c0e593174c9f211fc01f1
febeMina <140674479+febeMina@users.noreply.github.com>
2024-01-19 16:33:57 -0600

    CRUD peliculas

---
+++ app/Config/Routes.php
@@ -0,0 +1,34 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+ 
+ $routes->setDefaultNamespace('App\Controllers');
+ $routes->setDefaultController('Home');
+ $routes->setDefaultMethod('index');
+ $routes->setTranslateURIDashes(false);
+ $routes->set404Override();
+ 
+
+//$routes->get('/', 'Home::index');
+
+//$routes->get('pelicula', 'Pelicula::index');
+$routes->presenter('pelicula');
+
+
+
+
+
+
+//$routes->get('/index', 'Home::index'); //listado
+//$routes->get('/new', 'Home::new'); //Pintar el formulario
+//$routes->post('/create', 'Home::create'); //Procesar el formulario
+//$routes->get('edit/(:num)', 'Home::edit/$1'); //Pintar el formulario
+//$routes->put('update/(:num)', 'Home::update/$1'); //Procesar el formulario edicion
+//$routes->delete('delete/(:num)', 'Home::delete/$1'); //Elimina
+// $routes->get('/codeigniter4', 'Home::index');
+// $routes->get('/codeigniter', 'Home::code');
+
